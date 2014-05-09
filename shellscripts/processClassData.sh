ec97c5ff1ee3941762d9feb73ebaaae7064246ce
mangpo <mangpo@eecs.berkeley.edu>
2014-05-09 15:50:53 -0700

    delete some files

--- shellscripts/processClassData.sh
+++
@@ -1,8 +0,0 @@
-# 1) Set up separate directories for each class.
-# 2) Move files to those directories.
-# 3) Rename to something shorter and more uniform.
-# 4) Create a .csv file from each of the .sql files.
-
-buildClassList.py
-separateClassFiles.py
-
--- shellscripts/transformAllLogs.sh
+++
@@ -1,8 +0,0 @@
-#Tranform all json logs into nicely formatted csv logs.
-
-for log in `ls *.log`;
-do
-    echo $log
-    axis=${log:0:`expr length $log - 4`}.csv
-    echo transformOneLog.sh $log $axis
-done
